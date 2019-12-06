22971d97120e052078c4ce81921267bbf6ca1da9
lihuiminmn <1583314919@qq.com>
2019-12-06 09:08:21 +0800

    vue-swiper

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,26 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="foot">
+      <router-link tag="span" to="/home">首页</router-link>
+      <router-link tag="span" to="/my">我的</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.foot{
+   display: flex;
+   justify-content: space-around;
+   background: #f0f0f0;
+   height: 40px;
+   line-height: 40px;
+   position: fixed;
+   width: 100%;
+   left: 0;
+   bottom: 0;
+   span.router-link-active{
+     color:lightblue
+   }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,97 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <swiper :options="swiperOption" ref="mySwiper">
+      <swiper-slide>
+        <img src="http://dummyimage.com/100x50/red" alt />
+      </swiper-slide>
+      <swiper-slide>
+        <img src="http://dummyimage.com/100x50/ff0f0f" alt />
+      </swiper-slide>
+      <swiper-slide>
+        <img src="http://dummyimage.com/100x50/234bca" alt />
+      </swiper-slide>
+    </swiper>
+    <div class="nav">
+      <div class="item" v-for="(item,index) in data" :key="index">
+        <img :src="item.pic" alt />
+        <p>{{item.title}}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { swiper, swiperSlide } from "vue-awesome-swiper";
 
 export default {
-  name: 'home',
+  name: "home",
+  data() {
+    return {
+      data: [
+        {
+          pic: "http://dummyimage.com/100x100",
+          title: "仍无法为"
+        },
+        {
+          pic: "http://dummyimage.com/100x100",
+          title: "阿斯蒂芬"
+        },
+        {
+          pic: "http://dummyimage.com/100x100",
+          title: "认同与居"
+        },
+        {
+          pic: "http://dummyimage.com/100x100",
+          title: "热胎宝宝"
+        },
+        {
+          pic: "http://dummyimage.com/100x100",
+          title: "突然有机"
+        },
+        {
+          pic: "http://dummyimage.com/100x100",
+          title: "王企鹅服"
+        },
+        {
+          pic: "http://dummyimage.com/100x100",
+          title: "的无尽的"
+        },
+        {
+          pic: "http://dummyimage.com/100x100",
+          title: "其威锋网"
+        }
+      ],
+      swiperOption: {
+        autoplay: true
+      }
+    };
+  },
+  computed: {
+    swiper() {
+      return this.$refs.mySwiper.swiper;
+    }
+  },
   components: {
-    HelloWorld
+    swiper,
+    swiperSlide
+  }
+};
+</script>
+<style lang="scss">
+img {
+  width: 100%;
+}
+.nav{
+  display: flex;
+  flex-wrap: wrap;
+  .item{
+    width: 25%;
+    text-align: center;
+    padding: 10px 0;
+    img{
+      width: 60%;
+      border-radius:50%;
+    }
   }
 }
-</script>
+</style>
